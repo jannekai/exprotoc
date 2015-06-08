8967a80f7e6c1711818c91796d6b40c41e4cbca2
Janne Kaistinen <janne.kaistinen@iki.fi>
2015-06-08 17:17:07 +0300

    When assigning empty list to repeated element, delete key from hashdict

--- lib/exprotoc/generator.ex
+++ lib/exprotoc/generator.ex
@@ -249,7 +249,7 @@ defmodule Exprotoc.Generator do
     if ftype == :repeated do
       acc1 = acc1 <> """
 #{i}defp put_key(msg, #{fnum}, []) do
-#{i}  msg
+#{i}  HashDict.delete msg, #{f_num}
 #{i}end
 #{i}defp put_key(msg, #{fnum}, values) when is_list(values) do
 #{i}  HashDict.put msg, #{fnum}, { :repeated, values }
