e1b1d32df80ebb8946ab1a5f675c614b5e7e3684
Janne Kaistinen <janne.kaistinen@iki.fi>
2015-06-08 17:17:35 +0300

    Increase version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exprotoc.Mixfile do
 
   def project do
     [ app: :exprotoc,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0.0",
       compilers: [ :yecc, :erlang, :elixir, :app ],
       deps: deps ]
